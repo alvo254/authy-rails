c87c6b29805f19066e4783028b9d2533c508e835
alvo254 <alvinkhamron@gmail.con>
2022-10-25 19:46:15 +0300

    start

---
+++ config/routes.rb
@@ -0,0 +1,23 @@
+Rails.application.routes.draw do
+  post 'refresh', controller: :refresh, action: :create
+  post 'signin', controller: :signin, action: :create
+  post 'signup', controller: :signup, action: :create
+  delete 'signin', controller: :signin, action: :destroy
+
+  get 'me', controller: :users, action: :me
+
+  resources :todos
+  
+  resources :password_resets, only: [:create] do
+    collection do
+      get ':token', action: :edit, as: :edit
+      patch ':token', action: :update
+    end
+  end
+
+  namespace :admin do
+    resources :users, only: [:index, :show, :update] do
+      resources :todos, only: [:index], controller: 'users/todos'
+    end
+  end
+end
